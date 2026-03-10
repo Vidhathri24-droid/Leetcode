26cd7b30d93ecc0a8c4af3f66f0674b61bc4a201
Vidhathri <poluru.vidhathri1@gmail.com>
2026-03-10 16:55:45 +0000

    [LeetCode Sync] Runtime - 15 ms (29.97%), Memory - 13.4 MB (35.25%)

---
+++ 0268-missing-number/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            if i != nums[i]:
+                return i
+        if n not in nums:
+            return n
